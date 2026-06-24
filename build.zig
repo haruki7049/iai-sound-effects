1fb4f517e4cb0bc9dd80021ff2404a03699224cb
haruki7049 <tontonkirikiri@gmail.com>
2026-06-24 20:00:05 +0900

    Update build.zig

--- build.zig
+++ build.zig
@@ -5,26 +5,28 @@ pub fn build(b: *std.Build) anyerror!void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    try build_mod(b, target, optimize, b.path("src/accept.zig"), "accept.wav");
+}
+
+fn build_mod(
+    b: *std.Build,
+    target: std.Build.ResolvedTarget,
+    optimize: std.builtin.OptimizeMode,
+    lazypath: std.Build.LazyPath,
+    name: []const u8,
+) !void {
     // Dependencies
     const lightmix = b.dependency("lightmix", .{});
 
     // Modules
     const mod = b.createModule(.{
-        .root_source_file = b.path("src/accept.zig"),
+        .root_source_file = lazypath,
         .target = target,
         .optimize = optimize,
         .imports = &.{
             .{ .name = "lightmix", .module = lightmix.module("lightmix") },
         },
     });
-
-    // Static Library Install
-    const lib = b.addLibrary(.{
-        .linkage = .static,
-        .name = "iai-sound-effects",
-        .root_module = mod,
-    });
-    b.installArtifact(lib);
 
     // Library linking on Linux
     if (target.result.os.tag == .linux) {
@@ -33,12 +35,20 @@ pub fn build(b: *std.Build) anyerror!void {
         mod.linkSystemLibrary("libpipewire-0.3", .{});
     }
 
+    // Static Library Install
+    const lib = b.addLibrary(.{
+        .linkage = .static,
+        .name = name,
+        .root_module = mod,
+    });
+    b.installArtifact(lib);
+
     // Wave Install
     const wave = try l.addWave(b, mod, .{
         .format = .{ .wav = .{
             .bits = 16,
             .format_code = .pcm,
-            .name = "iai-sound-effects.wav",
+            .name = name,
         } },
     });
     l.installWave(b, wave);
